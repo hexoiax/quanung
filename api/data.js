570140d013838bd4303db22bc06bcda5d5a21059
hexoiax <68455865+hexoiax@users.noreply.github.com>
2026-05-15 14:56:46 +0700

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,20 @@
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7PMVjDBfldZjqDWHw_86xoVJVjwEgrOEJ-XyM0wykKKiZOqUif7Msx3goiFcasPwONQ/exec";
+
+export default async function handler(req, res) {
+  try {
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=120, stale-while-revalidate=300"
+    );
+
+    const response = await fetch(GOOGLE_SCRIPT_URL);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Cannot load landing data"
+    });
+  }
+}
